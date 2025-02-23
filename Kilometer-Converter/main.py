a514de4fa1c657f2587d47fcde01f6a0d67ec954
CieraBomer <cierabomer@westernwyoming.edu>
2025-02-23 21:32:20 +0000

    Ready for grading.

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,20 @@
 #
-# Name
-# Date
+# Ciera Bomer
+# 2-23-2025
 # Kilometer Converter Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# varibles
+CONVERSION_FACTOR = 0.6214
+show_miles = 0
+# Get the distance in kilometers
+def main ():
+    kilometers = float(input('Enter a distance in kilometers: '))
+    # Display the distance converted to miles
+    show_miles (kilometers)
+# Calculate miles
+def show_miles(km):
+    miles = km * CONVERSION_FACTOR
+    # Display the miles
+    print(km, 'Kilometers equals', format(miles, ',.3f'), 'miles.')
+main()

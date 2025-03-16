d8eaafc48bb108b76c32b1aabfdbef9b69a41b2c
CieraBomer <cierabomer@westernwyoming.edu>
2025-03-16 01:38:10 +0000

    Ready for grading.

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,14 @@
 #
-# Name
-# Date
+# Ciera Bomer
+# 3-15-2025
 # File Display Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Open the file
+myfile = open('numbers.txt', 'r')
+# Read and display the file's contents
+for line in myfile:
+    number = int(line)
+    print(number)
+# Close the file
+myfile.close()

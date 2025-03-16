e249d1a42bf8dad78685e25882c4dc57e48f79da
CieraBomer <cierabomer@westernwyoming.edu>
2025-03-16 01:50:21 +0000

    Ready for grading.

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,21 @@
 #
-# Name
-# Date
+# Ciera Bomer
+# 3-15-2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Varibles
+loop = 0
+total = 0
+average = 0
+# Open the file
+myfile = open('numbers.txt', 'r')
+# Read and average the number in the file
+for line in myfile:
+    total = total + int(line)
+    loop += 1
+average = total/loop
+# Print the the files average
+print ('The average is: ', average)
+# Close the file
+myfile.close()

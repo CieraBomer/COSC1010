4cde1853647a5790cd7afcef929dc3b86cc828da
CieraBomer <cierabomer@westernwyoming.edu>
2025-03-30 21:19:32 +0000

    Ready for grading

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -5,3 +5,23 @@
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+loop = 0
+total = 0
+average = 0
+# Open the file
+myfile = open('numbers.txt', 'r')
+# Read and average the number in the file
+# Also looks for ValueError and IOError
+try:
+    for line in myfile:
+        total = total + int(line)
+    loop += 1
+    average = total/loop
+    # Print the files average
+    print('The average is: ', average)
+except ValueError:
+    print("File must only have numbers.")
+except IOError:
+    print("Trouble opening file.")
+# Close the file
+myfile.close()

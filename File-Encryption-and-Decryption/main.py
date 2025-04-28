b7c6eb2592d8f374c9d3a7277d78f35904eeb6a5
CieraBomer <cierabomer@westernwyoming.edu>
2025-04-28 04:25:42 +0000

    Ready for Grading. Accidentally Commit & Pushed before finshing writing notes on what I did.

--- File-Encryption-and-Decryption/main.py
+++ File-Encryption-and-Decryption/main.py
@@ -11,6 +11,8 @@ codes = {'A':'!', 'B':'@','C':'#','D':'$','E':'%','F':'^','G':'&','H':'*','I':'K
         'e':'w','f':'e','g':'r','h':'t','i':'y','j':'u','k':'i','l':'o','m':'p','n':'a',
         'o':'s','p':'d','q':'f','r':'g','s':'h','t':'j','u':'k','v':'l','w':'z','x':'c',
         'y':'x','z':'v'}
+#Reads the contents of text.txt and encrypts it using my dictionary then sends it to the
+# encrpted.txt
 def encrypt_file (input_file_name, output_file_name, codes):
     try:
         with open(input_file_name, 'r') as infile:
@@ -30,6 +32,7 @@ def encrypt_file (input_file_name, output_file_name, codes):
         print(f"File '{input_file_name}' successfully encrypted to '{output_file_name}'.")
     except Exception as e:
         print (f"Error writing to output file '{output_file_name}':{e}")
+# Reads the encrpted file then decrypts it using my dictionary 
 def decrypt_file(input_file_name, codes):
     try:
         with open(input_file_name, 'r') as infile:
@@ -37,15 +40,18 @@ def decrypt_file(input_file_name, codes):
     except FileNotFoundError:
         print(f"Error: Input file '{input_file_name}' not found.")
         return
+# Reverses codes to character for decryption
     reverse_codes = {v: k for k, v in codes.items()}
     decrypted_text = ''
     for char in encrypted_text:
         if char in reverse_codes:
             decrypted_text += reverse_codes[char]
+# Keep the characters that aren't in the dictionary the same
         else:
             decrypted_text += char
     print('Decrypted text: ')
     print(decrypted_text)
+# Main function
 def main():
     input_file = 'text.txt'
     encrypted_file = 'encrypted.txt'
